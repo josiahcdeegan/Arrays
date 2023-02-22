6e86236c35f30511d6e8493294e108a810873838
josiahcdeegan <114314800+josiahcdeegan@users.noreply.github.com>
2023-02-22 11:10:57 -0500

    commit

---
+++ src/Arrays/Arrays.java
@@ -0,0 +1,14 @@
+package Arrays;
+
+import java.util.Scanner;
+
+public class Arrays {
+	public static void main(String[] args){
+		String[] name = {"Steve", "Freddy", "John", "Eddie", "Dave"};
+		int[] age = {35, 62, 43, 57, 48};
+		System.out.println(name[0] + " is " + age[0]);
+		System.out.println(name[1] + " is " + age[1]);
+		System.out.println(name[2] + " is " + age[2]);
+		System.out.println(name[3] + " is " + age[3]);
+		System.out.println(name[4] + " is " + age[4]);
+	}}
---
+++ src/Arrays/Arrays2.java
@@ -0,0 +1,7 @@
+package Arrays;
+
+public class Arrays2 {
+	public static void main(String[] args){
+		int[] number = {35, 62, 43, 57, 48};
+		System.out.println(number[5]);
+}}
